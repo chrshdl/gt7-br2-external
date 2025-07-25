c9f4f9e8438d03f1d361986d61edff92f5b08286
Christian Hedel <cwasilei@gmail.com>
2025-07-25 04:01:59 +0200

    workflow for Buildroot image build

---
+++ package/gt7-simdash/gt7-simdash.mk
@@ -0,0 +1,12 @@
+GT7_SIMDASH_VERSION = main  # Or tag/commit
+GT7_SIMDASH_SITE = https://github.com/chrshdl/gt7-simdash.git
+GT7_SIMDASH_SITE_METHOD = git
+
+GT7_SIMDASH_DEPENDENCIES = python3
+
+define GT7_SIMDASH_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install PREFIX=$(TARGET_DIR)/usr
+endef
+
+$(eval $(python-package))
+
